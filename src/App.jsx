34c7cc033fe33b516e7588139fb2bedb55123219
unknown <notnotsky26u@gmaile.com>
2025-03-30 12:46:31 +0530

    added proper modals ui but modal system still need to work out

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 console.clear();
 const { useState } = React;
 function App() {
-  const [isModal1Open, setisModal1Open] = useState(true);
+  const [isModal1Open, setisModal1Open] = useState(false);
+  const modal1Content = `
+  ಚಳ್ಳಕೆರೆ ನಗರಸಭಾ ಅಧ್ಯಕ್ಷರಾಗಿ ಮಂಜುಳಾ ಆರ್ ಪ್ರಸನ್ನ ಕುಮಾರ್ ನೇಮಕ
+  ಚಿತ್ರದುರ್ಗ ಜಿಲ್ಲೆ ಚಳ್ಳಕೆರೆ ನಗರಸಭಾ ಅಧ್ಯಕ್ಷರಾಗಿ ಶ್ರೀಮತಿ ಮಂಜುಳಾ ಆರ್
+  ಪ್ರಸನ್ನ ಕುಮಾರ್ ರವರು ಚಿತ್ರದುರ್ಗ ಜಿಲ್ಲೆಯ ವಿವಿಧ ತಾಲೂಕುಗಳಿಂದ ಆಗಮಿಸಿದ್ದ
+  ವಿಶ್ವಕರ್ಮ ಕುಲ ಬಾಂಧವರ ಅಪಾರವಾದ ಜನಸ್ತೋಮದೊಂದಿಗೆ *ದಿನಾಂಕ 26/03/2025 ನೇ
+  ಬುಧವಾರ ಪದವಿ ಪದಗ್ರಹಣ ಸ್ವೀಕರಿಸಿದರು ಕರ್ನಾಟಕದಲ್ಲಿ ಇದೊಂದು ಇತಿಹಾಸ
+  ಸೃಷ್ಟಿಯಾದ ದಿನ ಎಂದು ಹೇಳಬಹುದು ಇದುವರೆಗೂ ವಿಶ್ವಕರ್ಮ ಸಮಾಜದ ಹೆಣ್ಣುಮಗಳು ಈ
+  ಪದವಿಯನ್ನು ಸ್ವೀಕರಿಸಿರುವುದು ಇದೇ ಮೊದಲು ಎಂದು ಹೇಳಿದರು ತಪ್ಪಾಗಲಾರದು ಈ
+  ಒಂದು ಸುದಿನಕ್ಕೆ ನೆರೆದಿದ್ದ ಅಪಾರವಾದ ಜನರೇ ಸಾಕ್ಷಿಯಾಯಿತು ಅಧ್ಯಕ್ಷರಾಗಿ
+  ಪದವಿ ಸ್ವೀಕರಿಸಿರುವುದು ನಿಜಕ್ಕೂ ವಿಶ್ವಕರ್ಮ ಸಮಾಜವೇ ಹೆಮ್ಮೆ ಪಡುವಂತಾಗಿದ್ದು
+  ಶ್ರೀಮತಿ ಮಂಜುಳಾ ಆರ್ ಪ್ರಸನ್ ಕುಮಾರ್ ರವರಿಗೆ ರಾಜಕೀಯ ಜೀವನದ ಕ್ಷೇತ್ರದಲ್ಲಿ
+  ಇನ್ನೂ ಹೆಚ್ಚಿನ ಅಧಿಕಾರ ದೊಡ್ಡ ದೊಡ್ಡ ಹುದ್ದೆಗಳು ಸಿಗುವಂತಾಗಲಿ ಹಾಗೂ
+  ಜನಸೇವೆಯೇ ಜನಾರ್ದನ ಸೇವೆ ಎಂದು ತಿಳಿದು ಅವರು ನಡೆಯುವಂತಾಗಲಿ ಎಂದು ಕರ್ನಾಟಕ
+  ವಿಶ್ವಕರ್ಮ ಜನಸೇವಾ ಸಂಘ ಬೆಂಗಳೂರು *ರಾಜ್ಯಾಧ್ಯಕ್ಷರಾದ ಎಂ ಸೋಮಶೇಖರ್( ಕನ್ನಡ
+  ಸೋಮು) ಅತ್ತಿಬೆಲೆ. ರಾಜ್ಯ ಯುವ ಘಟಕ. ರಾಜ್ಯಾಧ್ಯಕ್ಷ ದೊಡ್ಡಬಳ್ಳಾಪುರ. ಸೋಮು
+  ವಿಶ್ವಕರ್ಮ ರಾಜ್ಯ ಪ್ರಧಾನ ಕಾರ್ಯದರ್ಶಿ ಹೊಸದುರ್ಗ ಪರಮೇಶ್ವರಚಾರ್ ಮಾರಬಘಟ್ಟ
+  ರಾಜ್ಯ ಕಾರ್ಯದರ್ಶಿ ಮಹಿಳಾ ವಿಭಾಗ ಕು.ಗಿರಿಜಾ ಸುರೇಶಚಾರ್ *ಮೆಟ್ಟಿನ
+  ಹೊಳೆ*ಹೊಸದುರ್ಗ ಇವರು ಶುಭಾಶಯ ಅಭಿನಂದನೆಗಳನ್ನು ಸಲ್ಲಿಸಿದ್ದಾರೆ
+  `;
   return (
     <div className="h-full w-full" id="masterContainer">
       <div
@@ -31,7 +50,9 @@ function App() {
         className="flex flex-wrap gap-2 justify-center items-center mt-12 w-full"
         id="articlesContainer"
       >
-        {isModal1Open && <Modal1 bannerUrl="./someImage.jpeg" />}
+        {isModal1Open && (
+          <Modal1 bannerUrl="./someImage.jpeg" content={modal1Content} />
+        )}
         <CardComp imgUrl="./someImage.jpeg" title="ಚಿತ್ರದುರ್ಗ" />
         <CardComp imgUrl="https://bit.ly/3zzCTUT" title="Example title" />
         <CardComp imgUrl="https://bit.ly/3zzCTUT" title="Example title" />
@@ -55,19 +76,19 @@ function App() {
       </div>
     </div>
   );
-  function Modal1({ bannerUrl }) {
+  function Modal1({ bannerUrl, content }) {
     return (
       <div
         aria-label="modal"
         className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-10 w-full h-full animate-fadeIn"
-      >
-        <div className="flex justify-start items-center flex-col relative w-full max-w-lg p-10 bg-white w-full h-full transition-all duration-300">
+      > 
+        <div className="overflow-scroll flex justify-start items-center flex-col relative w-full max-w-lg p-10 bg-white w-full h-full transition-all duration-300">
           <img
             src={bannerUrl}
             alt=""
-            className=" w-[100%] h-[35%] bg-cover rounded-lg"
+            className=" w-auto h-auto bg-cover rounded-lg"
           />
-            
+          {content}
           <div className="absolute cursor-pointer top-5 right-5">
             <svg
               xmlns="http://www.w3.org/2000/svg"
